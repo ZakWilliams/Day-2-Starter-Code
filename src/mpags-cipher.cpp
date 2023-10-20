6c9aa433f4061f2abb9246335a206b7e4bb7c8f5
ZakWilliams <zak.williams@bristol.ac.uk>
2023-10-20 10:15:14 +0100

    Zak Williams: Edit 2: function declaration moved to top, function definition moved to after 'main'.

--- src/mpags-cipher.cpp
+++ src/mpags-cipher.cpp
@@ -4,49 +4,7 @@
 #include <vector>
 
 //Transliteration function
-std::string transliteration(char in_char) {
-    std::string return_string{};
-    if (std::isalpha(in_char)) {
-        return_string = std::toupper(in_char);
-    }
-
-    // Transliterate digits to English words
-    if (std::isdigit(in_char)) {
-        switch (in_char) {
-            case '0':
-                return_string = "ZERO";
-                break;
-            case '1':
-                return_string = "ONE";
-                break;
-            case '2':
-                return_string = "TWO";
-                break;
-            case '3':
-                return_string = "THREE";
-                break;
-            case '4':
-                return_string = "FOUR";
-            break;
-            case '5':
-                return_string = "FIVE";
-                break;
-            case '6':
-                return_string = "SIX";
-                break;
-            case '7':
-                return_string = "SEVEN";
-                break;
-            case '8':
-                return_string = "EIGHT";
-                break;
-            case '9':
-                return_string = "NINE";
-                break;
-        }
-    }
-    return return_string;
-}
+std::string transliteration(const char in_char);
 
 int main(int argc, char* argv[])
 {
@@ -157,4 +115,48 @@ int main(int argc, char* argv[])
 
     // No requirement to return from main, but we do so for clarity and consistency
     return 0;
+}
+
+std::string transliteration(const char in_char) { //Transliterates numbers and capitalizes
+    std::string return_string{};
+    if (std::isalpha(in_char)) {
+        return_string = std::toupper(in_char);
+    }
+
+    // Transliterate digits to English words
+    if (std::isdigit(in_char)) {
+        switch (in_char) {
+            case '0':
+                return_string = "ZERO";
+                break;
+            case '1':
+                return_string = "ONE";
+                break;
+            case '2':
+                return_string = "TWO";
+                break;
+            case '3':
+                return_string = "THREE";
+                break;
+            case '4':
+                return_string = "FOUR";
+            break;
+            case '5':
+                return_string = "FIVE";
+                break;
+            case '6':
+                return_string = "SIX";
+                break;
+            case '7':
+                return_string = "SEVEN";
+                break;
+            case '8':
+                return_string = "EIGHT";
+                break;
+            case '9':
+                return_string = "NINE";
+                break;
+        }
+    }
+    return return_string;
 }
